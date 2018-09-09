9a0c81f7d3c01030de5651239f7f94799bf4baf5
lmh0506 <495453865@qq.com>
2018-09-09 20:03:56 +0800

    极速借贷  理财精选模块完成

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,18 +2,27 @@
   <div>
     <my-header></my-header>
     <slider></slider>
+    <novice></novice>
+    <borrow></borrow>
+    <money></money>
   </div>
 </template>
 
 <script>
 import MyHeader from '@/public/header.vue'
 import Slider from './hslider.vue'
+import Novice from './novice.vue'
+import Borrow from './borrow.vue'
+import Money from './money.vue'
 
 export default {
   name: 'Index',
   components: {
     MyHeader,
-    Slider
+    Slider,
+    Novice,
+    Borrow,
+    Money
   }
 }
 </script>
